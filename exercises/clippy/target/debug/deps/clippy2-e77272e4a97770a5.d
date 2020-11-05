8c24fa925f68a5640ae3159664c3a70f37b70715
Alexander Herlin <alexander.herlin@outlook.com>
2020-11-05 12:30:39 +0100

    Smart pointers - 5/11/2020

---
+++ exercises/clippy/target/debug/deps/clippy2-e77272e4a97770a5.d
@@ -0,0 +1,5 @@
+/Users/alexanderherlin/Repos/luncherna/rustlings/exercises/clippy/target/debug/deps/clippy2-e77272e4a97770a5.rmeta: clippy2.rs
+
+/Users/alexanderherlin/Repos/luncherna/rustlings/exercises/clippy/target/debug/deps/clippy2-e77272e4a97770a5.d: clippy2.rs
+
+clippy2.rs:
