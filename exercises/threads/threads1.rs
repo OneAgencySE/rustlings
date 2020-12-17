a812fbe15a73f52eb9879769f41018cc32148eec
Alexander Herlin <alexander.herlin@outlook.com>
2020-12-17 12:25:48 +0100

    17/12-2020: Mutex, Casting and more atomics

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -5,7 +5,6 @@
 // of "waiting..." and the program ends without timing out when running,
 // you've got it :)
 
-// I AM NOT DONE
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::{Arc, Mutex};
 use std::thread;
@@ -17,16 +16,19 @@ struct JobStatus {
 
 // TODO: Use Mutex
 fn main() {
-    let status = Arc::new(AtomicUsize::new(0));
+    let status = Arc::new(Mutex::new(0));
 
     let mut status_shared = Arc::clone(&status);
     thread::spawn(move || {
         for _ in 0..10 {
             thread::sleep(Duration::from_millis(250));
-            status_shared.fetch_add(1, Ordering::Relaxed);
+            // Lock -> get value -> mutex lock -> get -> update  -> unlock
+            let mut data = status_shared.lock().unwrap();
+            *data += 1
         }
     });
-    while status.load(Ordering::Relaxed) < 10 {
+
+    while *status.lock().unwrap() < 10 {
         println!("waiting... ");
         thread::sleep(Duration::from_millis(500));
     }
